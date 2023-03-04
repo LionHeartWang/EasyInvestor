88e6309996c96adfbf632a3417afb3108d0691b9
wangyiguang <LionHeartWang@outlook.com>
2023-03-04 16:31:25 +0800

    Create DataTypeException.java

---
+++ easyinvestor4j/src/main/java/org/lionheart/easyinvestor/exception/DataTypeException.java
@@ -0,0 +1,13 @@
+package org.lionheart.easyinvestor.exception;
+
+
+/**
+ * 数据异常。
+ *
+ * @author gufeng
+ */
+public class DataTypeException extends EasyInvestorException {
+    public DataTypeException(String message) {
+        super(message);
+    }
+}
