33b342cc158e2d9210a51f44cb15567c76808756
wangyiguang <LionHeartWang@outlook.com>
2023-03-04 16:29:01 +0800

    add EasyException

---
+++ easyinvestor4j/src/main/java/org/lionheart/easyinvestor/exception/EasyInvestorException.java
@@ -0,0 +1,13 @@
+package org.lionheart.easyinvestor.exception;
+
+
+/**
+ * 异常。
+ *
+ * @author gradywang
+ */
+public class EasyInvestorException extends RuntimeException {
+    public EasyInvestorException(String message) {
+        super(message);
+    }
+}
